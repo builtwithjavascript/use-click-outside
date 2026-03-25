a56daa9cab2ac86a04a4a191bc8f8cb3cdb6689d
damianof <damianof@users.noreply.github.com>
2026-03-25 10:59:26 -0400

    v1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export * from './use-click-outside/shared'
-export * from './use-click-outside/use-click-outside'
+export type { Fn, AnyFn } from './use-click-outside/shared'
+export { noop, isClient, isIOS } from './use-click-outside/shared'
+export type { OnClickOutsideOptions, OnClickOutsideHandler } from './use-click-outside/use-click-outside'
+export { useClickOutside } from './use-click-outside/use-click-outside'
